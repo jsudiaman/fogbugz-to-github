72c93e1032858bba987155e8331cc21bd7af8394
Jonathan Sudiaman <jsudiaman@gmail.com>
2017-03-14 19:33:06 -0400

    Update FBCheckin

--- src/main/java/com/sudicode/fb2gh/fogbugz/FBCheckin.java
+++ src/main/java/com/sudicode/fb2gh/fogbugz/FBCheckin.java
@@ -17,6 +17,9 @@ public class FBCheckin {
     private int revisionNumber;
     private String filename;
 
+    FBCheckin() {
+    }
+
     /**
      * @return The post-checkin revision number.
      */
