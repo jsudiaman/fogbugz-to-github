cf443d9f9bcdf0a272df1a43ceb480a47cce1e79
Jonathan Sudiaman <jsudiaman@gmail.com>
2019-07-10 02:29:53 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  cp -R target/apidocs public
+  cp -R target/apidocs/ public
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
